cf6b02bca6ae19cbd64ed2845440b77155906f24
= <=>
2019-07-04 11:51:54 -0400

    remove options from migrations

--- db/migrate/20190704154005_create_clients.rb
+++ db/migrate/20190704154005_create_clients.rb
@@ -2,7 +2,7 @@ class CreateClients < ActiveRecord::Migration[5.2]
   def change
     create_table :clients do |t|
       t.string :name
-      t.string :email, :unique => true
+      t.string :email
     end
   end
 end
--- db/migrate/20190704154150_create_appointments.rb
+++ db/migrate/20190704154150_create_appointments.rb
@@ -1,8 +1,8 @@
 class CreateAppointments < ActiveRecord::Migration[5.2]
   def change
     create_table :appointments do |t|
-      t.integer :barber_id, :foreign_key => true
-      t.integer :client_id, :foreign_key => true
+      t.integer :barber_id
+      t.integer :client_id
       t.string :notes
 
       t.timestamp :time_slot
