b2a90b00699304538fb15bd09e70120bf5b4bc34
= <=>
2019-07-04 15:40:55 -0400

    Adds validations for clients

--- app/models/client.rb
+++ app/models/client.rb
@@ -1,4 +1,7 @@
 class Client < ActiveRecord::Base
   has_many :appointments, dependent: :destroy
   has_many :barbers, through: :appointments
+
+  validates :name, presence: true
+  validates :email, presence: true, uniqueness: true
 end
