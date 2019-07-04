6cf68ea7de30211de789d9e1c1471254f700a140
= <=>
2019-07-04 11:46:52 -0400

    Add rule to ignore db files

---
+++ db/schema.rb
@@ -0,0 +1,31 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# Note that this schema.rb definition is the authoritative source for your
+# database schema. If you need to create the application database on another
+# system, you should be using db:schema:load, not running all the migrations
+# from scratch. The latter is a flawed and unsustainable approach (the more migrations
+# you'll amass, the slower it'll run and the greater likelihood for issues).
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 2019_07_04_154150) do
+
+  create_table "appointments", force: :cascade do |t|
+    t.integer "barber_id"
+    t.integer "client_id"
+    t.string "notes"
+    t.datetime "time_slot"
+  end
+
+  create_table "barbers", force: :cascade do |t|
+    t.string "name"
+  end
+
+  create_table "clients", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+  end
+
+end
