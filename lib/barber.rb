e8a857de681131153c239da94025891c5c824ee8
= <=>
2019-07-04 12:30:58 -0400

    Adds activerecord relationships

--- lib/barber.rb
+++ lib/barber.rb
@@ -1,3 +1,4 @@
 class Barber < ActiveRecord::Base
   has_many :appointments, dependent: :destroy
+  has_many :clients, through: :appointments
 end
--- lib/client.rb
+++ lib/client.rb
@@ -1,3 +1,4 @@
 class Client < ActiveRecord::Base
   has_many :appointments, dependent: :destroy
+  has_many :barbers, through: :appointments
 end
